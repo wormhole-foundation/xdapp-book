3e01373e1794d6b68589245629d832be7664dcd1
spacemandev <devbharel@gmail.com>
2022-07-01 01:08:35 -0500

    *sigh*

--- projects/wormhole-local-validator/evm.sh
+++ projects/wormhole-local-validator/evm.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Start EVM Chain 0
+npx pm2 kill -n evm0
+npx pm2 start 'ganache -p 8545 -m "myth like bonus scare over problem client lizard pioneer submit female collect" --block-time 1' --name evm0
+
 # Start EVM Chain 1
 npx pm2 kill -n evm1
 npx pm2 start 'ganache -p 8546 -m "myth like bonus scare over problem client lizard pioneer submit female collect" --block-time 1' --name evm1
@@ -11,6 +15,9 @@ cp .env.test .env
 npm run build
 
 # Deploy Wormhole Contracts to EVM Chain 0
+npm run migrate && npx truffle exec scripts/deploy_test_token.js && npx truffle exec scripts/register_solana_chain.js && npx truffle exec scripts/register_terra_chain.js && npx truffle exec scripts/register_eth_chain.js && npx truffle exec scripts/register_algo_chain.js && nc -lkp 2000 0.0.0.0
+
+# Deploy Wormhole Contracts to EVM Chain 1
 perl -pi -e 's/CHAIN_ID=0x2/CHAIN_ID=0x4/g' .env && perl -pi -e 's/8545/8546/g' truffle-config.js 
 npm run migrate && npx truffle exec scripts/deploy_test_token.js && npx truffle exec scripts/register_solana_chain.js && npx truffle exec scripts/register_terra_chain.js && npx truffle exec scripts/register_eth_chain.js && npx truffle exec scripts/register_algo_chain.js && nc -lkp 2000 0.0.0.0
 perl -pi -e 's/CHAIN_ID=0x4/CHAIN_ID=0x2/g' .env && perl -pi -e 's/8546/8545/g' truffle-config.js
