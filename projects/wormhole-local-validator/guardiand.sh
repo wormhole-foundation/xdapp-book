62fff2d81f3ab3651559585569bfbaa379a1eb3a
Csongor Kiss <ckiss@jumptrading.com>
2022-07-01 17:17:33 +0100

    Use prebuilt guardian docker image (unofficial for now)

--- projects/wormhole-local-validator/guardiand.sh
+++ projects/wormhole-local-validator/guardiand.sh
@@ -3,26 +3,23 @@
 
 set -euo pipefail
 
-GUARDIAND_FLAGS=
+# Unofficial guardian build for now
+DOCKER_IMAGE="ckiss123/guardiand"
+
+DOCKER_FLAGS=
 HOST=
 TERRAD_HOST=
 if [ "$(uname -m)" = "arm64" ]; then
-   GUARDIAND_FLAGS="-p 7070:7070 -p 7071:7071 -p 7073:7073 --platform linux/amd64"
+   DOCKER_FLAGS="-p 7070:7070 -p 7071:7071 -p 7073:7073 --platform linux/amd64"
    HOST="host.docker.internal"
    TERRAD_HOST="host.docker.internal"
 else
-   GUARDIAND_FLAGS="--network host"
+   DOCKER_FLAGS="--network host"
    TERRAD_HOST="terra-terrad"
 fi
 
-function cleanup {
-  docker kill guardiand 2>/dev/null || true
-  docker rm guardiand 2>/dev/null || true
-}
-trap cleanup EXIT
-
-docker run -d --name guardiand $GUARDIAND_FLAGS --hostname guardian-0 --cap-add=IPC_LOCK --entrypoint /guardiand guardian node \
-    --unsafeDevMode --guardianKey /tmp/bridge.key --publicRPC "[::]:7070" --publicWeb "[::]:7071" --adminSocket /tmp/admin.sock --dataDir /tmp/data \
+docker run --rm --name guardiand $DOCKER_FLAGS --hostname guardian-0 --cap-add=IPC_LOCK "$DOCKER_IMAGE" node \
+    --unsafeDevMode --guardianKey /bridge.key --publicRPC "[::]:7070" --publicWeb "[::]:7071" --adminSocket /admin.sock --dataDir /data \
     --ethRPC ws://$HOST:8545 \
     --ethContract "0xC89Ce4735882C9F0f0FE26686c53074E09B0D550" \
     --bscRPC ws://$HOST:8546 \
@@ -52,5 +49,3 @@ docker run -d --name guardiand $GUARDIAND_FLAGS --hostname guardian-0 --cap-add=
     --algorandAlgodToken "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" \
     --algorandAlgodRPC https://$HOST:4001 \
     --algorandAppID "4"
-
-docker logs guardiand -f
--- projects/wormhole-local-validator/wormhole.sh
+++ projects/wormhole-local-validator/wormhole.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 # Start guardiand
 
-npx pm2 stop guardiand
+npx pm2 stop guardiand 2> /dev/null || true
 npx pm2 start './guardiand.sh' --name guardiand
