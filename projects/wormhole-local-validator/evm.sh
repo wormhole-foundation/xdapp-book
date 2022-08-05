f5d290ad5931666a685b434bd4340a2965e8cee3
spacemandev <devbharel@gmail.com>
2022-08-05 02:13:23 -0500

    removed register algo from evm deploy as there was no register algo VAA in the env file. this will stop the error that everyone complains about

--- projects/wormhole-local-validator/evm.sh
+++ projects/wormhole-local-validator/evm.sh
@@ -3,11 +3,11 @@
 set -euo pipefail
 
 # Start EVM Chain 0
-npx pm2 stop evm0 2> /dev/null || true
+npx pm2 delete evm0 2> /dev/null || true
 npx pm2 start 'npx ganache -p 8545 -m "myth like bonus scare over problem client lizard pioneer submit female collect" --block-time 1' --name evm0
 
 # Start EVM Chain 1
-npx pm2 stop evm1 2> /dev/null || true
+npx pm2 delete evm1 2> /dev/null || true
 npx pm2 start 'npx ganache -p 8546 -m "myth like bonus scare over problem client lizard pioneer submit female collect" --block-time 1' --name evm1
 
 cd wormhole/ethereum
@@ -16,9 +16,9 @@ cd wormhole/ethereum
 make .env build
 
 # Deploy Wormhole Contracts to EVM Chain 0
-npm run migrate && npx truffle exec scripts/deploy_test_token.js && npx truffle exec scripts/register_solana_chain.js && npx truffle exec scripts/register_terra_chain.js && npx truffle exec scripts/register_eth_chain.js && npx truffle exec scripts/register_algo_chain.js && nc -lkp 2000 0.0.0.0
+npm run migrate && npx truffle exec scripts/deploy_test_token.js && npx truffle exec scripts/register_solana_chain.js && npx truffle exec scripts/register_terra_chain.js && npx truffle exec scripts/register_eth_chain.js
 
 # Deploy Wormhole Contracts to EVM Chain 1
 perl -pi -e 's/CHAIN_ID=0x2/CHAIN_ID=0x4/g' .env && perl -pi -e 's/8545/8546/g' truffle-config.js 
-npm run migrate && npx truffle exec scripts/deploy_test_token.js && npx truffle exec scripts/register_solana_chain.js && npx truffle exec scripts/register_terra_chain.js && npx truffle exec scripts/register_eth_chain.js && npx truffle exec scripts/register_algo_chain.js && nc -lkp 2000 0.0.0.0
+npm run migrate && npx truffle exec scripts/deploy_test_token.js && npx truffle exec scripts/register_solana_chain.js && npx truffle exec scripts/register_terra_chain.js && npx truffle exec scripts/register_eth_chain.js
 perl -pi -e 's/CHAIN_ID=0x4/CHAIN_ID=0x2/g' .env && perl -pi -e 's/8546/8545/g' truffle-config.js
--- projects/wormhole-local-validator/wormhole.sh
+++ projects/wormhole-local-validator/wormhole.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 # Start guardiand
 
-npx pm2 stop guardiand 2> /dev/null || true
+npx pm2 delete guardiand 2> /dev/null || true
 npx pm2 start './guardiand.sh' --name guardiand
