769eb5c509fd69fabe3d58c65c6975510558c03a
spacemandev <devbharel@gmail.com>
2022-08-14 19:13:56 -0600

    added docker checking to wormhole runner

--- projects/wormhole-local-validator/wormhole.bash
+++ projects/wormhole-local-validator/wormhole.bash
@@ -2,7 +2,13 @@
 
 set -euo pipefail
 
-# Start guardiand
+# Check if Docker is running
+if [ ! $(docker ps > /dev/null 2>&1) ]; then
+    echo "ERROR: Please start docker and try this command again."
+    exit 3
+fi
 
-npx pm2 delete guardiand 2> /dev/null || true
-npx pm2 start 'bash guardiand.bash' --name guardiand
+
+# Start guardiand
+#npx pm2 delete guardiand 2> /dev/null || true
+#npx pm2 start 'bash guardiand.bash' --name guardiand
