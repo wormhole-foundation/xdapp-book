c089283a115f8fb47beebdf03f3febb9b61cf2d6
Joe Howarth <josephehowarth@gmail.com>
2022-10-12 15:47:08 -0500

    fix spy_guardiand.bash hostname

--- projects/wormhole-local-validator/spy_guardiand.bash
+++ projects/wormhole-local-validator/spy_guardiand.bash
@@ -6,9 +6,16 @@ set -euo pipefail
 # dev.v2 for now (until we make a release)
 DOCKER_IMAGE="ghcr.io/certusone/guardiand:dev.v2"
 
+HOST=
+if [ "$(uname -m)" = "arm64" ]; then
+   HOST="host.docker.internal"
+else
+   HOST="localhost"
+fi
+
 docker run \
     --platform=linux/amd64 \
     -p 7073:7073 \
     --entrypoint /guardiand \
     ghcr.io/wormhole-foundation/guardiand:latest \
-spy --nodeKey /node.key --spyRPC "[::]:7073" --bootstrap /dns4/host.docker.internal/udp/8999/quic/p2p/12D3KooWL3XJ9EMCyZvmmGXL2LMiVBtrVa2BuESsJiXkSj7333Jw
+spy --nodeKey /node.key --spyRPC "[::]:7073" --bootstrap /dns4/${HOST}/udp/8999/quic/p2p/12D3KooWL3XJ9EMCyZvmmGXL2LMiVBtrVa2BuESsJiXkSj7333Jw
