ee885235a9041bd10e3299e07b6f79bda3d61f02
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-07-03 08:47:28 -0400

    WLV: adding ganache cli flag to provision enough accounts for token deploy script

--- projects/wormhole-local-validator/evm.bash
+++ projects/wormhole-local-validator/evm.bash
@@ -4,11 +4,11 @@ set -euo pipefail
 
 # Start EVM Chain 0
 npx pm2 delete evm0 2> /dev/null || true
-npx pm2 start 'npx ganache --chain.chainId 1 -p 8545 -m "myth like bonus scare over problem client lizard pioneer submit female collect" --block-time 1' --name evm0
+npx pm2 start 'npx ganache --wallet.totalAccounts=11 --chain.chainId 1 -p 8545 -m "myth like bonus scare over problem client lizard pioneer submit female collect" --block-time 1' --name evm0
 
 # Start EVM Chain 1
 npx pm2 delete evm1 2> /dev/null || true
-npx pm2 start 'npx ganache --chain.chainId 1397 -p 8546 -m "myth like bonus scare over problem client lizard pioneer submit female collect" --block-time 1' --name evm1
+npx pm2 start 'npx ganache --wallet.totalAccounts=11 --chain.chainId 1397 -p 8546 -m "myth like bonus scare over problem client lizard pioneer submit female collect" --block-time 1' --name evm1
 
 cd wormhole/ethereum
 
